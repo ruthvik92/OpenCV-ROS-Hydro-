2d5a71782a79f2e2c9e19c7ff514a2c4b4b769c4
ruthvik92 <ruthvik.nitc@gmail.com>
2015-02-05 21:29:48 +0530

    Create rename.cpp

---
+++ rename.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h> /////code to rename jpg files in a folder node_0_0.jpg to node00_0.jpg
+#include <opencv2/opencv.hpp>
+#include <sstream>
+#include<iostream>
+using namespace cv;
+using namespace std;
+int main( )
+{
+for(int j=0;j<=26;j++)
+{ stringstream ss1;
+	ss1<<j;
+
+for(int i=0;i<=10;i++)
+{
+ stringstream ss;          
+ ss<<i;                      
+ string si = "/home/iiith/Desktop/bundler/Dataset_side1_far/" +std::string("node_")+ss1.str()+std::string("_") + ss.str() + ".jpg"; ///jus change the path of your source ".jpg files///
+ cout <<" iteration value is:"<<ss.str()<<endl;
+Mat gray_image;
+Mat image;
+cout<<"path"<<si; 
+image = imread(si,1);
+if( !image.data )
+ {
+   printf( " No image data \n " );
+   printf("i:",i);
+   return -1;
+ }       
+string ssave1 = "/home/iiith/Desktop/editname/" + std::string("node0")+ss1.str()+std::string("_") +ss.str()+".jpg"; ///jus change the path of your destination ".pgm" files///
+imwrite( ssave1, image );
+ waitKey(100000);
+}
+}
+ return 0;
+
+}
