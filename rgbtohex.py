57d9e69d640c15a51090d9a76d7bbf56f34f8e20
ruthvik92 <ruthvik.nitc@gmail.com>
2015-04-11 12:13:55 +0530

    Update rgbtohex.py

--- rgbtohex.py
+++ rgbtohex.py
@@ -1,15 +1,17 @@
-f = open('/home/iiith/Desktop/python graphs/color_curve.txt', 'r')
-global lat
 def rgb_to_hex(rgb):
     return '#%02x%02x%02x' % rgb
-for line in f.read().split('//n'):
-    r,g,b=line.split('/s')
-    ##lat= line.split('//s')
-    print r
-    print rgb_to_hex(float(lat[0]),float(lat[1]),float(lat[2]))
 
-##thi converts the input file which has color codes in range of 0-255 to hex values.
-
-
-
- 
+with open('/home/iiith/Desktop/color_dataset_side3_far_rotation0.txt', 'r') as v:
+    for line in v:
+        if ' ' in line:
+            a = line.strip().split(' ')
+            a=rgb_to_hex((float(a[0]), float(a[1]), float(a[2]))) #arguement is a touple of 3 elements.
+            print a
+            f = open('/home/iiith/Desktop/hex/color_dataset_side3_far_rotation0.txt', 'a+')
+            print a
+            f.writelines(a+'\n')
+            f.flush()
+            
+                
+            
+##this code converts color code from rgb to hex 
