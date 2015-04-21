70713da574b6a77775e1fd8816b57a7cf417e350
ruthvik92 <ruthvik.nitc@gmail.com>
2015-04-21 18:08:24 +0530

    Update gpsholux.py

--- gpsholux.py
+++ gpsholux.py
@@ -4,33 +4,32 @@ def get_present_gps():
     ser=serial.Serial('/dev/ttyUSB0',4800)
     ser.open()
                     # open a file to write gps data
-    f = open('/home/iiith/Desktop/gps1.txt', 'a+')
-    data=ser.read(1024) # read 1024 bytes
+    f = open('/home/iiith/Desktop/gps1.txt', 'w')
+    data=ser.read(512) # read 1024 bytes
     f.write(data) #write data into file
-    f.flush() #flush from buffer into os buffer
+    #f.flush() #flush from buffer into os buffer
          #ensure to write from os buffers(internal) into disk
-    f = open('/home/iiith/Desktop/gps1.txt', 'a+')# fetch  the required file
+    f = open('/home/iiith/Desktop/gps1.txt', 'r')# fetch  the required file
     f1 = open('/home/iiith/Desktop/gps2.txt', 'a+')
     for line in f.read().split('\n'):
         if line.startswith('$GPGGA'):
-            
-            try:
+            print len(line)
+            if len(line)>50:
                 lat, _, lon= line.split(',')[2:5]
                 lat=float(lat)
-                lon=float(lon)
-                
+                lon=float(lon)    
                 print lat/100
                 print lon/100
                 a=[lat,lon]
-                f1.write(lat+",")
+                f1.write(str(lat)+",")
                 f1.flush()
-                f1.write(lon+"\n")
+                f1.write(str(lon)+"\n")
                 f1.flush()
-                f1.close()
-
-            except:
+            else:
                 pass
-
+            
+        
+        #f1.close()
 while True:
     get_present_gps()
     
